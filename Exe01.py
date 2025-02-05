f61a9babc1534f3cbc85d8ad3ccb47aaa8853839
GustavoWendt <gustavowendt14@gmail.com>
2025-02-05 13:13:14 -0300

    Alterações feitas.
    Adicionei 2 linhas de comentario de testes.

--- Exe01.py
+++ Exe01.py
@@ -6,3 +6,5 @@
 #Gosto de comer 
 #Minha cor favorita é verde (eu acho)
 #Eu amo a aula de zumba mas amo mesmo a de progamação de aplicativos
+#Teste para fixar o aprendizado da aula passada.
+#Teste de mais uma modificação para fixar o conhecimento da aula passada.
