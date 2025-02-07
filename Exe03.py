a767dfb3859f4e71242c49375e462bfac966927b
GustavoWendt <gustavowendt14@gmail.com>
2025-02-06 21:59:26 -0300

    exemplo 3

--- Exe03.py
+++ Exe03.py
@@ -1,1 +1,7 @@
 #exe03
+#Faça um programa que receba um salário de 1500 e tenha um aumento de 5% e ao final exiba o salário com o aumento
+x=1500
+print("Salário atual 1500")
+y=x * 0.05
+z=x+y
+print(z)
