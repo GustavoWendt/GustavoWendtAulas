a2d6fd79102717f114abba0bf26a97f5b958df49
GustavoWendt <gustavowendt14@gmail.com>
2025-02-19 21:45:40 -0300

    Exemplos-tuplas

---
+++ Exe01_tuplas.py
@@ -0,0 +1,7 @@
+vendas = ("Luan",'07/04/2008','15/03/2024',10000,'ADM')
+nome, data_contratacao, data_nascimento, salario, cargo=vendas
+print(nome)
+print(data_contratacao)
+print(data_nascimento)
+print(salario)
+print(cargo)
