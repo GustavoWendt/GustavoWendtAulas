b677c45451288bef0a061054be75bef69011271e
GustavoWendt <gustavowendt14@gmail.com>
2025-02-21 22:12:50 -0300

    Exemplos

---
+++ Exe001_leitura_TXT.py
@@ -0,0 +1,8 @@
+arquivo = open('C:/Users/Gustavo_Wendt150/Documents/Pasta_TXT/alunoTXT.txt','r')
+linhas = arquivo.readlines()
+print(linhas)
+
+
+arquivo = open('C:/Users/Gustavo_Wendt150/Documents/Pasta_TXT/Lista_compras.txt','r')
+linhas = arquivo.readlines()
+print(linhas)
---
+++ Exe003_espaços.py
@@ -0,0 +1,4 @@
+with open('C:/Users/Gustavo_Wendt150/Documents/Pasta_TXT/Resumo3.txt','w') as arquivo3:
+ arquivo3.write('Escrevendo no arquivo\n')
+ arquivo3.write('Capitão Stoltomayer\n')
+ print("Fim do programa")
